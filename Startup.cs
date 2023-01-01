83d8960c183e7249556d4968287c3dca4136eedb
kelesaoui <khalidelesaoui@yahoo.fr>
2023-01-02 00:50:15 +0100

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting ;
+using Microsoft.Extensions.Hosting;
 
 
 
